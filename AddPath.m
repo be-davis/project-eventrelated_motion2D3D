544cea438489d27fdce440714231aada6903b9ba
be-davis <brandon.davis9@gmail.com>
2018-07-25 18:10:53 -0700

    function for adding my repos

---
+++ AddPath.m
@@ -0,0 +1,8 @@
+function AddPath
+    % AddPath
+    % function for adding Brandon github repos to the path
+    addpath(genpath('/Volumes/Denali_DATA1/Brandon/code/git/mrC'));
+    addpath(genpath('/Volumes/Denali_DATA1/Brandon/code/git/rcaBase'));
+    addpath(genpath('/Volumes/Denali_DATA1/Brandon/code/git/export_fig'));
+end
+
